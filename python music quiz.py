0bbbba651cbe068b55899371f2f7fac2c25083be
zafira2705 <136670309+zafira2705@users.noreply.github.com>
2023-06-15 11:56:39 +0800

    Add files via upload

---
+++ python music quiz.py
@@ -0,0 +1,61 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================================== RESTART: C:\Users\PSC\AppData\Local\Programs\Python\Python311\pyhton music quiz.py ==================================
+permainan Teka Genre Muzik
+**************************
+Pilih genre lagu bagi setiap senikata lagu dibawah:
+-------------
+Tiada Arah Jodoh Kita:
+
+A.Balada
+B.Rock
+C.Kpop
+D.Nasyid
+sila pilih jawapan(A,B,C,D): a
+
+Maaf jawapan anda salah.Sila cuba lagi!
+Aialah jawapan yang tepat!
+-------------
+Kau Satu Satunya:
+
+A.Balada
+B.Rock
+C.Kpop
+D.Nasyid
+sila pilih jawapan(A,B,C,D): b
+
+Maaf jawapan anda salah.Sila cuba lagi!
+Bialah jawapan yang tepat!
+-------------
+Flower:
+
+A.Balada
+B.Rock
+C.Kpop
+D.Nasyid
+sila pilih jawapan(A,B,C,D): c
+
+Maaf jawapan anda salah.Sila cuba lagi!
+Cialah jawapan yang tepat!
+-------------
+Sepohon Kayu:
+
+A.Balada
+B.Rock
+C.Kpop
+D.Nasyid
+sila pilih jawapan(A,B,C,D): d
+
+Maaf jawapan anda salah.Sila cuba lagi!
+Dialah jawapan yang tepat!
+
+***********************
+         KEPUTUSAN       
+*************************
+Jawapan tepat:/ A/ B/ C/ D
+Tekaan:/ a/ b/ c/ d
+Jumlah soalan: 4
+Jawapan tepat: 0
+Markah yang dikumpul: 0.0 %
+**************************
